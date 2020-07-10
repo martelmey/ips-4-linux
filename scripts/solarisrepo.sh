5e8da264bbe03787d17867bc13828d2f9704ccc3
lattitude-cgi <martel.meyers@cgi.com>
2020-07-10 16:04:23 -0700

    setup solaris vm on oracle cloud, provision support repo w/ cert & key

--- scripts/solarisrepo.sh
+++ scripts/solarisrepo.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
+#ssh -i ssh-key-2020-07-10.key opc@150.136.183.223
+#scp -i ~/.ssh/ssh-key-2020-07-10.key pkg.oracle.com.certificate.pem opc@150.136.183.223:/export/home/opc
+#scp -i ~/.ssh/ssh-key-2020-07-10.key pkg.oracle.com.key.pem opc@150.136.183.223:/export/home/opc
+#mv /export/home/opc/* /root
+
+pkg set-publisher -k pkg.oracle.com.key.pem -c pkg.oracle.com.certificate.pem -G "*" -g https://pkg.oracle.com/solaris/support/ solaris
+pkg update --accept
 
 zfs create rpool/export/repoSolaris11
